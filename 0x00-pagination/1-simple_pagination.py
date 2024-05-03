13cc80dbe4edb5d378b87893f020256ab827eb69
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-03 18:54:39 +0200

    Paginate large csv file

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Module of simple pagination
+"""
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """
+    Function that calculates index range
+    """
+    return (page_size * (page - 1), page_size * page)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Get page data
+        """
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+        start, end = index_range(page, page_size)
+        if start > len(self.dataset()) - 1 or end > len(self.dataset()) - 1:
+            return []
+        return self.dataset()[start:end]
