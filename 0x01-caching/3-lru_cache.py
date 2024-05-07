4a40359ed4ea82bc747c01808e982b65d1ded684
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-07 23:50:45 +0200

    Implement a LRU cache

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Implementation of a LRU cache
+"""
+import time
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    LRUCache implements a simple LRU cache
+    """
+    def __init__(self):
+        """ init function """
+        super().__init__()
+        self.lru = {}
+
+    def put(self, key, item):
+        """ Adds the new item to the dictionary """
+        if key is not None and item is not None:
+            max_size = LRUCache.MAX_ITEMS
+            self.cache_data[key] = item
+            self.lru[key] = time.time()
+        if len(self.cache_data) > max_size:
+            lru_time = 0
+            discard_key = None
+            for k, v in self.lru.items():
+                if k != key and time.time() - v > lru_time:
+                    lru_time = time.time() - v
+                    discard_key = k
+            print("DISCARD:", discard_key)
+            del self.lru[discard_key]
+            del self.cache_data[discard_key]
+
+    def get(self, key):
+        """ Returns the value of the key from the dictionary """
+        if key is None:
+            return None
+        try:
+            self.lru[key] = time.time()
+            return self.cache_data[key]
+        except KeyError:
+            del self.lru[key]
+            return None
