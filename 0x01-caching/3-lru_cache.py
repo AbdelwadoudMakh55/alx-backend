2be3b0e5b77afd577121110cf8e4d53b569fdfde
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-08 13:11:08 +0200

    Implement a LRU Cache

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -2,8 +2,6 @@
 """
 Implementation of a LRU cache
 """
-import math
-import time
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -14,32 +12,29 @@ class LRUCache(BaseCaching):
     def __init__(self):
         """ init function """
         super().__init__()
-        self.lru = {}
+        self.lru = []
 
     def put(self, key, item):
         """ Adds the new item to the dictionary """
         if key is not None and item is not None:
             max_size = LRUCache.MAX_ITEMS
             self.cache_data[key] = item
-            self.lru[key] = time.time()
+            if key not in self.lru:
+                self.lru.append(key)
+            else:
+                self.lru.pop(self.lru.index(key))
+                self.lru.append(key)
         if len(self.cache_data) > max_size:
-            lru_time = math.inf
-            discard_key = None
-            for k, v in self.lru.items():
-                if k != key and v < lru_time:
-                    lru_time = v
-                    discard_key = k
+            discard_key = self.lru.pop(0)
             print("DISCARD:", discard_key)
-            del self.lru[discard_key]
             del self.cache_data[discard_key]
 
     def get(self, key):
         """ Returns the value of the key from the dictionary """
         if key is None:
             return None
-        try:
-            self.lru[key] = time.time()
+        if key in self.cache_data.keys():
+            self.lru.pop(self.lru.index(key))
+            self.lru.append(key)
             return self.cache_data[key]
-        except KeyError:
-            del self.lru[key]
-            return None
+        return None
