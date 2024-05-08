b895e4d9dd764c546c148fb63fa6bbdfe409e508
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-08 13:28:12 +0200

    Implement a MRU Cache

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -2,7 +2,6 @@
 """
 Implementation of a MRU cache
 """
-import time
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -13,32 +12,29 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """ init function """
         super().__init__()
-        self.mru = {}
+        self.mru = []
 
     def put(self, key, item):
         """ Adds the new item to the dictionary """
         if key is not None and item is not None:
             max_size = MRUCache.MAX_ITEMS
             self.cache_data[key] = item
-            self.mru[key] = time.time()
+            if key not in self.mru:
+                self.mru.append(key)
+            else:
+                self.mru.pop(self.mru.index(key))
+                self.mru.append(key)
         if len(self.cache_data) > max_size:
-            mru_time = 0
-            discard_key = None
-            for k, v in self.mru.items():
-                if k != key and v > mru_time:
-                    mru_time = v
-                    discard_key = k
+            discard_key = self.mru.pop(-2)
             print("DISCARD:", discard_key)
-            del self.mru[discard_key]
             del self.cache_data[discard_key]
 
     def get(self, key):
         """ Returns the value of the key from the dictionary """
         if key is None:
             return None
-        try:
-            self.mru[key] = time.time()
+        if key in self.mru:
+            self.mru.pop(self.mru.index(key))
+            self.mru.append(key)
             return self.cache_data[key]
-        except KeyError:
-            del self.mru[key]
-            return None
+        return None
