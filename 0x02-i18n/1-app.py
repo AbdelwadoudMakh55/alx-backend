e4945b86a481af29637e2db2290335872c515056
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-14 15:43:53 +0200

    Config the app from Config class

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,6 +3,7 @@
 Simple Flask app with jinja template
 """
 
+
 from flask import Flask, render_template
 from flask_babel import Babel
 
@@ -14,17 +15,17 @@ babel = Babel(app)
 class Config:
     """ Class for configuration """
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCAL = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app.config["BABEL_DEFAULT_LOCAL"] = "en"
-app.config["BABEL_DEFAULT_TIMEZONE"] = "utc"
-app.config["LANGUAGES"] = Config.LANGUAGES
+app.config.from_object(Config)
 
 
 @app.route("/")
 def hello_holberton():
     """ Function that displays the Jinja template """
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
