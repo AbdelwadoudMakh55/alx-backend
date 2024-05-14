c0d3f6fe8a5d9bcdf67d184b9f8a8aea6ea620cc
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-14 14:57:30 +0200

    Create a simple flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+Simple Flask app with jinja template
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_holberton():
+    """ Function that displays the Jinja template """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
