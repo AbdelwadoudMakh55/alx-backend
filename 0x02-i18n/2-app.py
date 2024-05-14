bf6079a61606af35141e21467f91b79852b1fa39
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-14 15:54:15 +0200

    Add language selection

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Simple Flask app with jinja template and flask_babel for i18n
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ Class for configuration """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ Get accepted languages from the browser requests header """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def hello_holberton():
+    """ Function that displays the Jinja template """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
