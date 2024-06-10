8bdb02199b7a30ae1f1cf91543ec77df06c70648
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-06-10 18:33:55 +0100

    Connect to Redis using NodeJS

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,7 @@
+const { createClient } = require('redis');
+async function connectToRedis() {
+  const client = createClient();
+  client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
+  client.on('connect', () => console.log('Redis client connected to the server'));
+}
+connectToRedis();
